c2b36d3eda774994b8462fb7150fd489dd190005
Nicolas Labey <nicolas@labey.fr>
2023-03-29 11:44:21 +0200

    clean

--- AOC2002.Program/Program.cs
+++ AOC2002.Program/Program.cs
@@ -1,9 +1,1 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using AocModel;
-
-Console.WriteLine("Advent of code 2022!");
-
-Console.WriteLine("Day 4");
-int result = new Day4().SolvePart1();
-Console.WriteLine("result: " + result);
-Console.ReadLine();
